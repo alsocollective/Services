e723a2b282f5a21b95a34b1a108e89730c6d0925
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2014-11-10 14:33:17 -0500

    made it respond as jsonp

--- application/application/views.py
+++ application/application/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, render_to_response, get_object_or_404, redirect
 from django.http import HttpResponse
-from django.utils import simplejson
+import json,re
+
 
 
 def ip(request):
-	data = simplejson.dumps({"host": request.META.get('REMOTE_ADDR')})
-	return HttpResponse(data, mimetype='application/json')
+	data = json.dumps({"data":{"host": request.META.get('REMOTE_ADDR')}})
+	data = "%s(%s);"%(request.META['PATH_INFO'].split("/")[-1].split("=")[1],data)
+	
+	return HttpResponse(data, mimetype='application/json',content_type="application/json")
 
+	
