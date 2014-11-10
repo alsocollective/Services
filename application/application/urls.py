a761a75d2aff32107aac4fc564b9c63cea4ea311
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2014-11-10 12:42:33 -0500

    added the single functionality i wanted

--- application/application/urls.py
+++ application/application/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'application.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^ip/', 'application.views.ip', name='ip'),
+    # url(r'^admin/', include(admin.site.urls)),
 )
