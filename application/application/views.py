73ce139e6c8aaa7a3c857c4ad18621eb1044e7f5
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2014-11-10 14:35:30 -0500

    made it respond as jsonp

--- application/application/views.py
+++ application/application/views.py
@@ -5,9 +5,9 @@ import json,re
 
 
 def ip(request):
-	data = json.dumps({"data":{"host": request.META.get('REMOTE_ADDR')}})
+	data = json.dumps({"host": request.META.get('REMOTE_ADDR')})
 	data = "%s(%s);"%(request.META['PATH_INFO'].split("/")[-1].split("=")[1],data)
-	
+
 	return HttpResponse(data, mimetype='application/json',content_type="application/json")
 
 	
