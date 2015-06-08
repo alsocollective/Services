4e18c05ad4173c441277e99dc9229298217cd5d2
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2015-06-08 13:23:58 -0400

    added a index page

--- application/application/urls.py
+++ application/application/urls.py
@@ -4,10 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'application.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^ip/', 'application.views.ip', name='ip'),
-    url(r'^loc/','application.views.country', name='loc')
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^loc/','application.views.country', name='loc'),
+    url(r'^.*/$', 'application.views.allother')
 )
--- application/application/views.py
+++ application/application/views.py
@@ -3,6 +3,27 @@ from django.http import HttpResponse
 import json,re,requests
 
 
+def allother(request):
+	out = """
+		<h1>ALSO Services</h1>
+		<a href="http://alsocollective.com">did you mean to go to our main site?</a>
+		<br>
+		<br>
+		<p>if requesting content from this site either go to /loc or /ip</p>
+		<p>both of the services are require to be quired with a callback functoin</p>
+		<code>
+		$.getJSON("http://examplewebsite.com/ip/&callback=?", function(data){ <br>
+			console.log(data);	<br>
+			GLOBAL.getLocation(data.host); <br>
+		});
+		</code>
+		<h2>/loc</h2>
+		<p>returns your location in a json</p>
+		<h2>/ip</h2>
+		<p>returns your IP address</p>
+	"""
+	return HttpResponse(out)
+
 # We simply return the users ip addres to them as a function
 # this requires a call back, example bellow
 '''
